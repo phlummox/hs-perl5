36569a448b31ba9fe881f8ac8e2542506d491296
Audrey Tang <audreyt@audreyt.org>
2015-08-15 14:55:01 +0800

    * Add installation instructions from Haskell Stack

--- Pugs/src/Pugs/Version.hs
+++ Pugs/src/Pugs/Version.hs
@@ -17,7 +17,7 @@
 #define PUGS_VERSION "6.2.13.dev"
 #endif
 #ifndef PUGS_DATE
-#define PUGS_DATE "Today"
+#define PUGS_DATE "20150815"
 #endif
 #ifndef PUGS_SVN_REVISION
 #define PUGS_SVN_REVISION 0
@@ -33,7 +33,7 @@ name       = "Perl6 User's Golfing System"
 versnum    = PUGS_VERSION
 date       = PUGS_DATE
 version    = name ++ ", version " ++ versnum ++ ", " ++ date ++ revision
-copyright  = "Copyright 2005-2013, The Pugs Contributors"
+copyright  = "Copyright 2005-2015, The Pugs Contributors"
 revnum     = show (PUGS_SVN_REVISION :: Integer)
 revision
     | rev <- revnum
