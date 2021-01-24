b22d1d097f000cc19d58e7d6806edef4b1ccf94c
phlummox <phlummox@localhost>
2021-01-24 10:29:07 +0000

    fix linker errors, update cabal file
    TODO: currently libraries "perl dl m" etc in perl_configure.sh are
    hard-coded -- extract them from perl output instead.

--- perl_configure.sh
+++ perl_configure.sh
@@ -9,6 +9,8 @@ OUTFILE=hs-perl5.buildinfo
 echo > $OUTFILE
 echo "cc-options: " $cc_options   >>  $OUTFILE
 echo "ld-options: " $ld_options   >>  $OUTFILE
+## TODO: extract this from perl output
+echo "extra-libraries: perl dl m pthread c crypt" >> $OUTFILE
 
 #echo "result buildinfo is: " >&2
 #cat -n $OUTFILE >&2
